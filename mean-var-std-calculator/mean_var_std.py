f04ac6fd459669041b5f5a0d6b00855f9c6a6a72
jasonyauw777 <57711222+jasonyauw777@users.noreply.github.com>
2022-04-14 11:38:43 +0700

    Add files via upload

---
+++ mean-var-std-calculator/mean_var_std.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def calculate(list):
+    if len(list) != 9:
+      raise ValueError("List must contain nine numbers.")
+    #Convert list to array and reshape into 3X3 matrix
+    matrix_array = np.array(list).reshape((3,3))
+    #Nested Dictionaries
+    calculations = {
+      "mean": [
+        np.mean(matrix_array, axis=0).tolist(),
+        np.mean(matrix_array, axis=1).tolist(),
+        np.mean(matrix_array).tolist()
+      ],
+
+      "variance": [
+        np.var(matrix_array, axis=0).tolist(),
+        np.var(matrix_array, axis=1).tolist(),
+        np.var(matrix_array).tolist()
+      ],
+
+      "standard deviation": [
+        np.std(matrix_array, axis=0).tolist(),
+        np.std(matrix_array, axis=1).tolist(),
+        np.std(matrix_array).tolist()
+      ],
+
+      "max": [
+        np.max(matrix_array, axis=0).tolist(),
+        np.max(matrix_array, axis=1).tolist(),
+        np.max(matrix_array).tolist()
+      ],
+
+      "min": [
+        np.min(matrix_array, axis=0).tolist(),
+        np.min(matrix_array, axis=1).tolist(),
+        np.min(matrix_array).tolist()
+      ],
+
+      "sum": [
+        np.sum(matrix_array, axis=0).tolist(),
+        np.sum(matrix_array, axis=1).tolist(),
+        np.sum(matrix_array).tolist()
+      ]
+    }
+    return calculations
